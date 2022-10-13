1c98ca0a02c6e1d6a6b585f4908bf9dcfdb5b570
curjlync <curjlync@ut.utm.edu>
2022-10-13 17:43:23 -0500

    Library implementedish

--- library.cpp
+++ library.cpp
@@ -1,1 +1,92 @@
 #include "library.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+void print_movie(movie to_print);
+
+library::library() {
+  
+}
+
+library::~library() {
+
+}
+
+void library::insert_sorted(movie new_movie) {
+  movies.push_back(new_movie);
+}
+
+void library::find_movie(std::string title) {
+  for (movie& m : movies) {
+    if(m.title.find(title) != std::string::npos) {
+      print_movie(m);
+    }
+  }
+}
+
+void library::director_search(std::string director) {
+  for (movie& m : movies) {
+    if(m.director == director) {
+      print_movie(m);
+    }
+  }
+}
+
+void library::remove(std::string title) {
+  movies.remove_if([&](movie m){return m.title == title;});
+}
+
+void library::read_from_file(std::string path_to_file) {
+  std::ifstream file;
+  file.open(path_to_file);
+  std::string title, director, format, runtime, year, price;
+
+  std::getline(file, title, ',');
+  while(file){
+    std::getline(file, director, ',');
+    std::getline(file, runtime, ',');
+    std::getline(file, format, ',');
+    std::getline(file, price, ',');
+    std::getline(file, year, ',');
+
+    movie m = {title, director, std::stoi(runtime), format, std::stof(price), std::stoi(year)};
+    insert_sorted(m);
+
+    file.get();
+    std::getline(file, title, ',');
+  }
+  file.close();
+}
+
+void library::write_to_file(std::string path_to_file) {
+  std::ofstream file;
+  file.open(path_to_file);
+  for (movie& m : movies) {
+    file << m.title << ','
+	 << m.director << ','
+	 << m.runtime << ','
+	 << m.format << ','
+	 << m.price << ','
+	 << m.year << ",\n";
+  }
+  file.close();
+}
+
+void library::push_front(movie new_movie) {
+  movies.push_front(new_movie);
+}
+
+void library::push_back(movie new_movie) {
+  movies.push_back(new_movie);
+}
+
+void print_movie(movie to_print) {
+  std::cout << "Title: " << std::setw(20) << std::left << to_print.title
+	    << "  Director: " << std::setw(15) << std::left << to_print.director
+	    << "  Runtime: " << std::setw(5) << std::left << to_print.runtime
+	    << "  Format: " << std::setw(7) << std::left << to_print.format
+	    << "  Price: " << std::setw(5) << std::left << to_print.price
+	    << "  Year: " << std::setw(4) << std::left << to_print.year << std::endl;
+}
--- library.h
+++ library.h
@@ -1,5 +1,32 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+#include <list>
+#include <string>
 
+struct movie {
+  std::string title;
+  std::string director;
+  int runtime;
+  std::string format;
+  float price;
+  int year;
+};
+
+class library {
+ private:
+  std::list<movie> movies;
+
+ public:
+  library();
+  ~library();
+  void insert_sorted(movie new_movie);
+  void find_movie(std::string title);
+  void director_search(std::string director);
+  void remove(std::string title);
+  void read_from_file(std::string path_to_file);
+  void write_to_file(std::string path_to_file);
+  void push_front(movie new_movie);
+  void push_back(movie new_movie);
+};
 #endif //LIBRARY_H
