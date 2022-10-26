e479b05392bb65dbb313ab481e8bad9a79a6c6d8
stegarala <stegarc@ut.utm.edu>
2022-10-25 19:23:04 -0500

    doq added

--- library.cpp
+++ library.cpp
@@ -14,9 +14,10 @@
 #include <string>
 
 void library::insert_sorted(movie new_movie) {
-  
-  std::list<movie>::iterator itr;
-  itr = movies.begin();    
+
+  //iterator that points to the beginning of the  list
+  std::list<movie>::iterator itr = movies.begin();
+  //loop that will keep pushing the itr forward until it reaches the end of list or movie being inserted has value is less than itr->title
   while(itr != movies.end() && new_movie.title.compare(itr->title) > 0){
     itr++;
   }
@@ -24,15 +25,19 @@ void library::insert_sorted(movie new_movie) {
 }
 
 void library::find_movie(std::string title) {
+  //checks all movies
   for (movie& m : movies) {
+    //if title is found as a sub-string then print the movie info
     if(m.title.find(title) != std::string::npos) {
       print_movie(m);
     }
   }
 }
 
-void library::director_search(std::string director) {
+void library::director_search(std::string director){
+  //checks all movies
   for (movie& m : movies) {
+    //if director is found then return the movie info
     if(m.director == director) {
       print_movie(m);
     }
@@ -40,12 +45,14 @@ void library::director_search(std::string director) {
 }
 
 void library::remove(std::string title) {
+  //searches the list from beginning to end, if title is found then it will removed
   movies.remove_if([&](movie m){return m.title == title;});
 }
 
 void library::read_from_file(std::string path_to_file) {
   std::ifstream file;
   file.open(path_to_file);
+  //will read everything as a string then convert runtime & year to int and price to float 
   std::string title, director, format, runtime, year, price;
   
   std::getline(file, title, ',');
@@ -55,10 +62,11 @@ void library::read_from_file(std::string path_to_file) {
     std::getline(file, format, ',');
     std::getline(file, price, ',');
     std::getline(file, year, ',');
-    
+    //puts info into a movie struct 'm' to pass into the inserted_sorteted function
     movie m = {title, director, std::stoi(runtime), format, std::stof(price), std::stoi(year)};
     insert_sorted(m);
-    
+
+    //get will help with newline character so that info is read in correctly
     file.get();
     std::getline(file, title, ',');
   }
